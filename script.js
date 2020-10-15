33c4ddbfc025123384bdc525adeef3364a75c509
Jessica Mangiameli <jmangiam@asu.edu>
2020-10-15 10:17:58 -0500

    generator functions

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ function generatePassword() {
 
   var special = confirm("Would you like special characters in your new password?");
 }
+
+// *************** //
+
+// functions for generator
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
