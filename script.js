111490af453a3129cf37473ff37836ec38932a3f
Jessica Mangiameli <jmangiam@asu.edu>
2020-10-15 12:24:56 -0500

    basic code needs to be cleaned up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Add event listener to generate button
 
-document.querySelector("generate").addEventListener("click", writePassword);
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Arrays for password conditions
 
@@ -28,11 +28,11 @@ function generatePassword() {
 
 while(confirmLength <= 7 || confirmLength >= 129) {
     alert("Password length must be between 8-128 characters. Please try again.");
-    var confirmLength = (prompt("How many chracters woukd you like to use for your password?"));
+    var confirmLength = (prompt("How many chracters would you like to use for your password?"));
     }
 
 // Repeat how many characters the user has chosen
-alert ('Your password will have ${confirmLength} chracters.');
+alert (`Your password will have ${confirmLength} chracters.`);
 
 // Password parameters
 var confirmLowerCase = confirm ("Click OK if you would like to include lowercase letters.");
@@ -46,11 +46,40 @@ var confirmSpecialCharacter = confirm ("Click OK if you would like to include sp
       var confirmUpperCase = confirm ("Click OK if you would like to include uppercase letters.");
       var confirmNumericCharacter = confirm ("Click OK if you would like to include numbers.");
       var confirmSpecialCharacter = confirm ("Click OK if you would like to include special characters.");
-    }
+  }
+
+// Action parameters
+
+var passwordCharacters = []
+
+if (confirmLowerCase) {
+  passwordCharacters = passwordCharacters.concat(lower)
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+if (confirmUpperCase) {
+  passwordCharacters = passwordCharacters.concat(upper)
+}
+
+if (confirmNumericCharacter) {
+  passwordCharacters = passwordCharacters.concat(number)
+}
+
+if (confirmSpecialCharacter) {
+  passwordCharacters = passwordCharacters.concat(symbol)
+}
+
+console.log (passwordCharacters)
+
+// For loop
+
+var randomPassword = ""
+
+for (var i = 0; i < confirmLength; i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  console.log(randomPassword)
+  }
+  return randomPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
