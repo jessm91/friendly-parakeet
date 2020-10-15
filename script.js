47eaef0c0fb9beaea0e123a10a8e115551a1f07d
Jessica Mangiameli <jmangiam@asu.edu>
2020-10-15 09:41:21 -0500

    password variables

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +13,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompts displayed after button is clicked
+function generatePassword() {
+  var length = prompt("Please choose a character length between 12 and 128 for your new password");
+  
+  var lowerCase = confirm("Would you like lowercase letters in your new password?");
+
+  var upperCase = confirm("Would you like uppercase letters in your new password?");
+
+  var number = confirm("Would you like numbers in your new password?");
+
+  var special = confirm("Would you like special characters in your new password?");
+}
